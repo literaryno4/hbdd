35141e50cbc47f75175121dca7b10c57d60be051
literaryno4 <literaryno4@gmail.com>
2023-07-07 14:58:25 +0800

    add bgm

--- script.js
+++ script.js
@@ -78,3 +78,20 @@ function fallEmoji(emoji) {
 
   drop();
 }
+
+function toggleMusic() {
+  var music = document.getElementById('bgMusic');
+  var playButton = document.getElementById('playButton');
+  var pauseButton = document.getElementById('pauseButton');
+
+  if (music.paused) {
+    music.play();
+    playButton.style.display = 'none';
+    pauseButton.style.display = 'block';
+  } else {
+    music.pause();
+    playButton.style.display = 'block';
+    pauseButton.style.display = 'none';
+  }
+}
+
